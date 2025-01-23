6a5a0768fa94f088ca24fbfab529b285a41e8f9e
Cyber Netha <120327906+cybernetha@users.noreply.github.com>
2025-01-23 20:43:28 +0530

    main.js

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/fi
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyC8KbWGMA24BIgt1ud274Psyphm2Mf6COQ",
+  authDomain: "chat-foxee.firebaseapp.com",
+  databaseURL: "https://chat-foxee-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-foxee",
+  storageBucket: "chat-foxee.firebasestorage.app",
+  messagingSenderId: "531579135295",
+  appId: "1:531579135295:web:70b333e2bfdcecf4229f8a"
 };
 
 // Initialize Firebase
